86581e4b25713f1d5ca554b60776bb91b0c88bea
yunsuper <yunsuper80@gmaile.com>
2025-10-16 17:28:31 +0900

    기초뼈대 만들기

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+// dotenv 모듈
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.listen(process.env.PORT);
+
+
+const userRouter = require("./routes/users");
+const bookRouter = require("./routes/books");
+const cartRouter = require("./routes/carts");
+const likeRouter = require("./routes/likes");
+const orderRouter = require("./routes/orders");
+
+app.use("/users", userRouter);  
+app.use("/books", bookRouter);  
+app.use("/carts", cartRouter);  
+app.use("/likes", likeRouter);  
+app.use("/orders", orderRouter);
+  
---
+++ routes/likes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+router.use(express.json());
+
+// 좋아요 추가
+router.post("/:id", (req, res) => {
+    res.json("좋아요 추가");
+});
+
+// 좋아요 취소
+router.delete("/:id", (req, res) => {
+    res.json("좋아요 취소");
+});
+
+module.exports = router;
